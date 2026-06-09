5219ede78981c410f345d961b8f174a1c4fd7355
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2026-06-09 00:14:45 -0700

    swithc to workers?

--- functions/pa-js/plausible.js
+++
@@ -1,3 +0,0 @@
-export async function onRequestGet() {
-	return fetch("https://analytics.byseansingh.com/js/pa-hKo0_jTmBVNnhdPbwQxsT.js");
-}
---
+++ worker.js
@@ -0,0 +1,19 @@
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+
+		if (url.pathname === "/pa-js/plausible.js") {
+			return fetch("https://analytics.byseansingh.com/js/pa-hKo0_jTmBVNnhdPbwQxsT.js");
+		}
+
+		if (url.pathname === "/pa-api/event") {
+			return fetch("https://analytics.byseansingh.com/api/event", {
+				method: request.method,
+				headers: request.headers,
+				body: request.body,
+			});
+		}
+
+		return env.ASSETS.fetch(request);
+	},
+};
