54156998fdea3045183e8d766d7fccd3b3a38cd3
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2026-06-09 00:32:46 -0700

    whatever

--- worker.js
+++ worker.js
@@ -3,7 +3,15 @@ export default {
 		const url = new URL(request.url);
 
 		if (url.pathname === "/cdn/sdk.js") {
-			return fetch("https://analytics.byseansingh.com/js/pa-hKo0_jTmBVNnhdPbwQxsT.js");
+			const res = await fetch("https://analytics.byseansingh.com/js/pa-hKo0_jTmBVNnhdPbwQxsT.js");
+			const text = await res.text();
+			const patched = text.replaceAll("https://analytics.byseansingh.com/api/event", "/pa-api/event");
+			return new Response(patched, {
+				headers: {
+					"Content-Type": "application/javascript",
+					"Cache-Control": "public, max-age=3600",
+				},
+			});
 		}
 
 		if (url.pathname === "/pa-api/event") {
