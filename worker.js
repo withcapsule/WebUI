cfbed6c5de829408a281422837fdc855c3ca4f8d
Sean Singh <sean.9s9s0@gmail.com>
2026-06-18 15:59:52 -0700

    Revert "temporary debug"
    This reverts commit b6b2bb5e050270b532ae753b7125da680c73ae2c.

--- worker.js
+++ worker.js
@@ -3,29 +3,18 @@ export default {
 		const url = new URL(request.url);
 
 		if (url.pathname === "/cdn/d") {
-			return new Response(
-				JSON.stringify(
-					{
-						cf_connecting_ip: request.headers.get("CF-Connecting-IP"),
-						x_forwarded_for: request.headers.get("X-Forwarded-For"),
-						cf_ipcountry: request.headers.get("CF-IPCountry"),
-						cf: {
-							country: request.cf?.country,
-							region: request.cf?.region,
-							city: request.cf?.city,
-						},
-					},
-					null,
-					2,
-				),
-				{
-					status: 200,
-					headers: {
-						"Content-Type": "application/json",
-						"Cache-Control": "no-store",
-					},
-				},
+			const headers = new Headers(request.headers);
+			headers.set(
+				"X-Forwarded-For",
+				request.headers.get("CF-Connecting-IP") || "",
 			);
+			headers.delete("host");
+
+			return fetch("https://analytics.byseansingh.com/api/event", {
+				method: request.method,
+				headers,
+				body: request.body,
+			});
 		}
 
 		if (url.pathname === "/u.js") {
